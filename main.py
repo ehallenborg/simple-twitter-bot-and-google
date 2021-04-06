b380e6752229e7722058526549b9759c4c106662
ehallenborg <ehallenborg@gmail.com>
2021-04-05 20:24:01 -0400

    temp fix for google api request limits

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import gspread
 from twitter import *
 import json
+import time
 
 def send_tweet(event, context):
     # set credentials
@@ -20,15 +21,33 @@ def send_tweet(event, context):
     # get movies
     movies = []
     val = 'No'
+    number = list(range(2, wks.row_count))
+    batches = [number[x:x+10] for x in range(0, len(number), 10)]
 
-    for r in range(2, wks.row_count - 1):
-        row = wks.row_values(r)
-        if row:
-            if wks.acell(f"D{r}").value.capitalize() == val:
-                movies.append(row)
-                wks.update(f"D{r}", 'Yes')
-        else:
+    run_throughs = 0
+
+    print(batches)
+    empty = False
+    for batch in batches:
+        for r in batch:
+            row = wks.row_values(r)
+            print(row)
+            if row:
+                if wks.acell(f"D{r}").value.capitalize() == val:
+                    movies.append(row)
+                    wks.update(f"D{r}", 'Yes')
+            else:
+                empty = True
+                break
+        if empty:
             break
+        else:
+            time.sleep(10)
+            run_throughs += 1
+
+        if run_throughs == 5:
+            time.sleep(15)
+            run_throughs = 0
 
     if movies:
         for i in range(len(movies)):
